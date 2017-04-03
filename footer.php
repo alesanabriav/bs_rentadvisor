a202db359101a1f9d4a80172cb6c2884e02aa137
Alejandro Sanabria <alesanabriav@gmail.com>
2017-04-03 16:23:30 -0500

    get res

--- footer.php
+++ footer.php
@@ -1,5 +1,18 @@
 	<!--wordpress files-->
 	  <?php wp_footer() ?>
 	<!-- /wordpress files-->
+	<script>
+		$.({
+			url: '/wp-admin/admin-ajax.php',
+			type: 'post',
+			data: {
+				action: 'store_contact',
+				data: {name: 'ale'}
+			}
+		})
+		.done(function(res) {
+			console.log(res);
+		})
+	</script>
 </body>
 </html>
