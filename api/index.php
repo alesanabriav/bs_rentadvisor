f231987f43b2591e82549b5a364ecdfd1d734394
Alejandro Sanabria <alesanabriav@gmail.com>
2017-04-03 16:16:36 -0500

    store_contact

--- api/index.php
+++ api/index.php
@@ -5,10 +5,10 @@
 **/
 
 
-add_action( 'wp_ajax_nopriv_bs_store_contact', 'bs_store_contact' );
-add_action( 'wp_ajax_bs_store_contact', 'bs_store_contact' );
+add_action( 'wp_ajax_nopriv_store_contact', 'store_contact' );
+add_action( 'wp_ajax_store_contact', 'store_contact' );
 
-function bs_store_contact() {
+function store_contact() {
 		// $data = $_POST['data'];
 		// $res = storeContact($data);
 		header('Content-type: application/json');
